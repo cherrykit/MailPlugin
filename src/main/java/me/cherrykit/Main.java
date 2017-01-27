6a1151c4892d083866544571ea073cd70a83dc31
Cherry <cherrykit@outlook.com>
2017-01-27 10:11:52 +0100

    Improved code structure

--- src/main/java/me/cherrykit/Main.java
+++ src/main/java/me/cherrykit/Main.java
@@ -27,62 +27,63 @@ public class Main extends JavaPlugin {
 			
 			//Registers type of command
 			String type;
-            try {
-            	type = args[0];
-            } catch (Exception e) {
-            	type = "";
-            }
+			try {
+				type = args[0];
+			} catch (Exception e) {
+				type = "";
+			}
 			
-            //Differs between send, clear and read
-            switch (type) {
-            
-            case "send" :
-            	
-            	try { 
-            		String addressee = args[1];
-            		String message = "";
-            		
-            		//Turn remaining args into one string
-            		int length = args.length;
-            		for (int i = 2; i < length; i++) {
-            			message = message + args[i] + " ";
-            		}
-            		
-            		Mail.saveMail(addressee, message, p.getName());
-            		
-            		p.sendMessage(ChatColor.GREEN + "Your mail has been sent.");
-            		
-            	} catch (Exception e) {
-            		p.sendMessage("Syntax: /mail send <player> <message>");
-            	}
-            	
-            	break;
-            	
-            case "clear" :
-            	
-            	Mail.deleteMail(p.getName());
-            	p.sendMessage(ChatColor.GREEN + "Your mail has been deleted.");
-            	
-            	break;
-            	
-            case "read" :
-            	
-            	ArrayList<String> mail = Mail.getMail(p.getName());
-            	
-            	//Sends player a message for each mail
-            	if (!mail.isEmpty()) {
-            		for (int i = 0; i < mail.size(); i++) {
-                		p.sendMessage(ChatColor.GOLD + mail.get(i));
-                	}
-            	}
-            	
-            	break;
-            	
-            default:
-            	
-            	p.sendMessage(ChatColor.RED + "Syntax: /mail <read/clear/send> (player) (message)");
-            	
-            }
+			//Differs between send, clear and read
+			switch (type) {
+				//If command was /mail send
+				case "send" :
+					
+					try { 
+						//Get addressee
+						String addressee = args[1];
+						String message = "";
+						
+						//Turn remaining args into one string (the message)
+						int length = args.length;
+						for (int i = 2; i < length; i++) {
+							message = message + args[i] + " ";
+						}
+						
+						//Saves mail in database
+						Mail.saveMail(addressee, message, p.getName());
+						p.sendMessage(ChatColor.GREEN + "Your mail has been sent.");
+					
+					} catch (Exception e) {
+						p.sendMessage("Syntax: /mail send <player> <message>");
+					}
+					
+					break;
+				
+				//If command was /mail clear
+				case "clear" :
+					//Deletes mail
+					Mail.deleteMail(p.getName());
+					p.sendMessage(ChatColor.GREEN + "Your mail has been deleted.");
+					
+					break;
+				
+				//If command was /mail read
+				case "read" :
+					//Gets mail from database
+					ArrayList<String> mail = Mail.getMail(p.getName());
+					
+					//Sends player a message for each mail
+					if (!mail.isEmpty()) {
+						for (int i = 0; i < mail.size(); i++) {
+							p.sendMessage(ChatColor.GOLD + mail.get(i));
+						}
+					}
+					
+					break;
+				
+				default:
+					p.sendMessage(ChatColor.RED + "Syntax: /mail <read/clear/send> (player) (message)");
+			}
 			return true;
 		}
 		return false;
